9f158536852775b92f87d1941477aa1b8966e1eb
Aman Tiwari <amantiwari@Amans-MacBook-Pro.local>
2021-10-06 13:36:35 +0530

    fix; some changes

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is a test project for CI/CD</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is a test project for CI/CD: #2</p>
       </header>
     </div>
   );
